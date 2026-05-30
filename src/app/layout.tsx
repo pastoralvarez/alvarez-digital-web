5f165fc60a6cb061ed28dc6b400d721762f1a35f
Pastor Alvarez <alvarez261093@gmail.com>
2026-05-30 16:51:22 -0400

    fix google fonts import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             crossOrigin="anonymous"
           />
         )}
+        <link href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&family=DM+Sans:ital,opsz,wght@0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;1,9..40,400&display=swap" rel="stylesheet" />
       </head>
       <body className="min-h-screen antialiased">{children}</body>
     </html>
